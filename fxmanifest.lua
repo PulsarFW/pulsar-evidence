b32c38d98f336af96289516cec7c0bbff3faec1e
Artmines <arturowells@gmail.com>
2026-04-27 23:53:48 -0230

    feat(init):Initial version

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version("cerulean")
+games({ "gta5" }) 
+lua54("yes")
+client_script("@pulsar-core/exports/cl_error.lua")
+client_script("@pulsar-pwnzor/client/check.lua")
+server_script("@oxmysql/lib/MySQL.lua")
+
+description("Sandbox RP Framework Evidence System")
+name("Sandbox RP Framework: pulsar-evidence")
+author("Dr Nick")
+version("v1.0.0")
+
+server_scripts({
+	'@oxmysql/lib/MySQL.lua',
+	"server/**/*.lua",
+})
+
+client_scripts({
+	"client/**/*.lua",
+})
+
+shared_scripts({
+	"shared/**/*.lua",
+})
